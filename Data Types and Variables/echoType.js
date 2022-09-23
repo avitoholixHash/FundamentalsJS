0e5c71d1a85bb1287741a4bb2525f3a1b7520ef7
avitoholixHash <114178116+avitoholixHash@users.noreply.github.com>
2022-09-23 20:55:56 +0300

    new task from home work

--- Data Types and Variables/echoType.js
+++ Data Types and Variables/echoType.js
@@ -1,1 +1,21 @@
+function echoType(arg) {
 
+    if (typeof arg === 'string') {
+
+        console.log(typeof arg);
+        console.log(arg);
+
+    } else if (typeof arg === 'number') {
+
+        console.log(typeof arg);
+        console.log(arg);
+
+    }else{
+
+        console.log(typeof arg);
+        console.log('Parameter is not suitable for printing');
+
+    }
+
+}
+echoType(null);
